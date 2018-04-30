06d9dbb16a9eb94441468415a717c533c8510f25
Ryan <rhight618@gmail.com>
2018-04-29 23:29:31 -0500

    updates to controller to add search and delete

--- src/main/java/naggingann/MedicationRepository.java
+++ src/main/java/naggingann/MedicationRepository.java
@@ -1,10 +1,25 @@
 package naggingann;
 
 import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface MedicationRepository extends CrudRepository<Medication, Long> {
+	
+    @Query(value = "SELECT * FROM medication m where m.name = :name", nativeQuery=true)
+    public Optional<Medication> findByName(@Param("name") String name);
+    
+    @Transactional
+    @Modifying
+    @Query(value = "delete from medication where id = :Id", nativeQuery=true)
+    void delete(@Param("Id") Long Id);
+     
 
 }
--- src/main/java/naggingann/NaggingAnnController.java
+++ src/main/java/naggingann/NaggingAnnController.java
@@ -1,5 +1,7 @@
 package naggingann;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,11 +32,23 @@ public class NaggingAnnController {
 		medicationRepository.save(med);
 		return "Saved";
 	}
+	
+	@GetMapping(path="/delete") // Map ONLY GET Requests
+	public @ResponseBody String addNewMedication (@RequestParam(name="id", required=true) int id) {
+		medicationRepository.delete(Long.valueOf(id));
+		return "Medication Deleted";
+	}
 
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<Medication> getAllUsers() {
 		// This returns a JSON or XML with the users
 		return medicationRepository.findAll();
+	}
+	
+	@GetMapping(path="/search")
+	public @ResponseBody Optional<Medication> getMedicationsByName(@RequestParam(name="name", required=false) String name) {
+		// This returns a JSON or XML with the Meds
+		return medicationRepository.findByName(name);
 	}
 	
 	
